b5318b696d14e308ac568ed7506127073295d6d7
Luis Mora <luis.iarmolen@gmail.com>
2025-04-14 19:23:59 -0500

    eleven

--- next.config.mjs
+++ next.config.mjs
@@ -1,5 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  // Configuración para imágenes
   images: {
     domains: ['bsrvywfejjtrrcobljvv.supabase.co'],
     remotePatterns: [
@@ -10,6 +11,24 @@ const nextConfig = {
         pathname: '/storage/v1/object/public/**',
       },
     ],
+  },
+  
+  // Asegurarse de que las rutas sean case-insensitive
+  async rewrites() {
+    return [
+      {
+        source: '/servicios',
+        destination: '/servicios',
+      },
+      {
+        source: '/Servicios',
+        destination: '/servicios',
+      },
+      {
+        source: '/SERVICIOS',
+        destination: '/servicios',
+      }
+    ];
   },
 };
 
